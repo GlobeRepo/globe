9ba5218d6cb307973f300e5176cb66ada0878626
Kamronbek Rakhimov <156855800+rkamron@users.noreply.github.com>
2025-01-01 15:17:07 -0500

    user profile with auth js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 // src/app/page.tsx
 
+import SignIn from '@/components/sign-in';
 import Globe from '../components/globe';  // Import the Globe component
 
 const Page = () => {
   return (
     <main style={{ margin: 0, padding: 0 }}>
       <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Welcome to My Travel Globe</h1>
-      <Globe />  {/* Render the Globe component */}
+      <SignIn />
     </main>
   );
 };
