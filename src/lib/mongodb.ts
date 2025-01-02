700eed7f033794675a966f6c342e0678b2b2055d
Kamronbek Rakhimov <156855800+rkamron@users.noreply.github.com>
2025-01-02 18:21:03 -0500

    added mongodb basic connection, next commit will have it setup with oauth

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,30 @@
+import { MongoClient } from "mongodb";
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+const uri = process.env.MONGODB_URI as string;
+const options = {};
+
+if (!uri) {
+  throw new Error("Please add your MongoDB URI to .env.local");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} 
+// Else if in production
+else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
