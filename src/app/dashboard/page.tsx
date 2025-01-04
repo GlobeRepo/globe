64c98d18d116fdc86a65ee6c2250171f8680cea2
corebenj <168385529+corebenj@users.noreply.github.com>
2025-01-03 23:29:55 -0500

    welcome page updates and styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,25 @@
 export default function Page() {
     return <p>Dashboard Page</p>
+        // <div className="wrapper">
+    //   <form action="">
+    //     <title>Login Page | MyTravelGlobe</title>
+    //     <h1>Login</h1>
+    //     <div className="input-box">
+    //       <input type="text" placeholder = "Username" required />
+    //       {/* <FaUser className="icon" /> */}
+    //     </div>
+    //     <div className="input-box">
+    //       <input type="password" placeholder = "Password" required />
+    //       {/* <FaUser className="icon" /> */}
+    //     </div>
+    //     <div className="remember-forgot">
+    //       <label><input type="checkbox" /> Remember me </label>
+    //       <a href="#">Forgot Password?</a>
+    //     </div>
+    //     <button type="submit">Login</button>
+    //     <div className="register-link">
+    //       <p>Need to register? <a href="#">Register</a></p>
+    //     </div>
+    //   </form>
+    // </div>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,45 @@
+import React from 'react';
+import './page.css';
+import Head from 'next/head';
+// import { FaUser, FaLock } from "react-icons/fa";
+
 // src/app/page.tsx
 
 import Globe from '../components/globe';  // Import the Globe component
 
-const Page = () => {
+
+
+const MainPage = () => {
   return (
-    <main style={{ margin: 0, padding: 0 }}>
-      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Welcome to My Travel Globe</h1>
-      <Globe />  {/* Render the Globe component */}
-    </main>
+    <>
+      <Head>
+        <title>MyTravelGlobe</title>
+      </Head>
+      <header className="navbar">
+          <div className="navbar-container">
+            <div className="welcome-heading">My Travel Globe</div>
+            <nav className="nav-links">
+              <a href="#" className="links">About</a>
+              <a href="#" className="links">Contact</a>
+            </nav>
+          </div>
+      </header>
+      <main className="login-page">        
+        <div className="container">
+          <div className="prompt-group">
+            <p className="intro-text">Where have you been and where should you go? We will show you a new way to experience the world!</p>
+          </div>
+
+          <div className="button-group">
+            <button className="btn signup">Sign Up</button>
+            <button className="btn login">Log in</button>
+          </div>
+        </div>
+        <p>stuff to be determined</p>
+      </main>
+    </>
+
   );
 };
 
-export default Page;
+export default MainPage;
